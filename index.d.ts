0b29fcc83b97f5f2585852281acb8d186227d56f
Wink Saville <wink@saville.com>
2017-04-07 20:17:46 -0700

    Update to latest from the DefinitelyTyped/types/webassembly-js-api
    See [PR webassembly-js-api](https://github.com/DefinitelyTyped/DefinitelyTyped/pull/15662)

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
-/**
- * WebAssembly v1 (MVP) declaration file for TypeScript
- * Definitions by: 01alchemist (https://twitter.com/01alchemist)
- *                 and Wink Saville <wink@saville.com>
- */
+// Type definitions for WebAssembly v1 (MVP)
+// Project: https://github.com/winksaville/test-webassembly-js-ts
+// Definitions by: 01alchemist <https://twitter.com/01alchemist>, Wink Saville <wink@saville.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+/**
+ * The WebAssembly namespace, see [WebAssembly](https://github.com/webassembly)
+ * and [WebAssembly JS API](http://webassembly.org/getting-started/js-api/)
+ * for more information.
+ */
 declare namespace WebAssembly {
     /**
      * WebAssembly.Module
